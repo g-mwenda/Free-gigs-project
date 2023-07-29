d99af8f379479230fe0ae1f17a3fc96ef74566e6
Jesse <jessemutua76@gmail.com>
2023-07-29 19:59:50 +0300

    Conversation and messaging

--- free_gigz/app/controllers/messages_controller.rb
+++ free_gigz/app/controllers/messages_controller.rb
@@ -1,47 +1,48 @@
 class MessagesController < ApplicationController
-    
-    ##get messages
-    def index 
-        messages= Message.all
-        render json: messages
+    # List all messages for a conversation
+    def index
+      conversation = Conversation.find(params[:conversation_id])
+      messages = conversation.messages
+      render json: messages
     end
-
-    ##get messages with id only
+  
+    # Show a message
     def show
-        message=Message.find_by(id: params[:id])
-        if message
-            render json: message, status: :ok
-        else
-            render json: {error: "Response not found" }, status: :not_found
-        end
+      message = Message.find(params[:id])
+      render json: message
     end
-
-    ##creating a new message
+  
+    # Create a new message
     def create
-        message = Message.new(message_params)
-        if message.save
-            render json: message, status: :created
-        else
-            render json: {error: "Unprocessable entity"}, status: :unprocessable_entity
-        end
+      message = Message.new(message_params)
+      if message.save
+        render json: message, status: :created
+      else
+        render json: message.errors, status: :unprocessable_entity
+      end
     end
-
+  
+    # Edit a message
+    def update
+      message = Message.find(params[:id])
+      if message.update(message_params)
+        render json: message, status: :ok
+      else
+        render json: message.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # Delete a message
     def destroy
-        message = Message.find(params[:id])
-        if user.id = message.client_id
-            message.destroy
-            head :no_content
-        else
-            render json: {error: "You cannot delete a message that is not yours"}, status: :not_authorised
-        end
-
-    end 
-
-
-
-    private
-    def message_params
-        params.require(:message).permit(:content, :client_id, :freelancer_id, :conversation_id)
+      message = Message.find(params[:id])
+      message.destroy
+      head :no_content
     end
-
-end
+  
+    private
+  
+    def message_params
+      params.require(:message).permit(:conversation_id, :client_id_id, :freelancer_id, :content)
+    end
+  end
+  
