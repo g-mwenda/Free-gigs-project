fe39a8af150ca15770bd607cd2d1d45e3d31b5e5
Thomas <thomaskambale1@gmail.com>
2023-08-11 18:32:00 +0300

    deployment changes

--- free_gigz/app/controllers/application_controller.rb
+++ free_gigz/app/controllers/application_controller.rb
@@ -2,7 +2,23 @@ class ApplicationController < ActionController::API
      include ActionController::Cookies
      
      before_action :authorize
-  
+  # config/application.rb
+module FreeGigs
+   class Application < Rails::Application
+     config.load_defaults 6.1
+ 
+     # This is set in apps generated with the --api flag, and removes session/cookie middleware
+     config.api_only = true
+ 
+     # ▾ Must add these lines! ▾
+     # Adding back cookies and session middleware
+     config.middleware.use ActionDispatch::Cookies
+     config.middleware.use ActionDispatch::Session::CookieStore
+ 
+     # Use SameSite=Strict for all cookies to help protect against
+     config.action_dispatch.cookies_same_site_protection = :strict
+  end
+end
      
      def authorize
         @current_user=User.find_by(id: session[:user_id])
